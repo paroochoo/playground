c63a39133c4dfffa911e1527bca5646b42fb789e
paroochoo <paroochoo@github.com>
2016-04-18 10:52:03 +0200

    add test in default location

---
+++ src/test/java/playground/JUnit5DefaultTest.java
@@ -0,0 +1,13 @@
+
+package playground;
+
+import org.junit.gen5.api.Test;
+
+public class JUnit5DefaultTest
+{
+	@Test
+	void helloDefaultJUnit()
+	{
+		System.out.println("Hello JUnit5 in default location!");
+	}
+}
